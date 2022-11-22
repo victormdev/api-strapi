f5eebceaae704ff593b02060a1a6c3234d7ac2db
Victor Macedo <victoralexandrino99@gmail.com>
2022-11-22 06:31:35 -0300

    update admin

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
+  },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', '77b2c87dbab4e1697bec244226fbd1b3'),
   },
 });
