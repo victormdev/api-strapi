a159978e01862ea05c3d3d4d1f0687b4eb688e3d
Victor Macedo <victoralexandrino99@gmail.com>
2022-11-21 22:48:24 -0300

    changes

--- config/server.js
+++ config/server.js
@@ -3,6 +3,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['gTthbjSBwGS8WlSQexyB+A==','70MYsbNeOP10hJA31pEP/w==','2r5fLxa4lGV1TSoWrzdiLQ==','CGMSTuM0N7D68kq/+rfpmw=='])
+    keys: env.array('APP_KEYS', ['gTthbjSBwGS8WlSQexyB+A==','70MYsbNeOP10hJA31pEP/w==','2r5fLxa4lGV1TSoWrzdiLQ==','CGMSTuM0N7D68kq/+rfpmw==']),
   },
 });
